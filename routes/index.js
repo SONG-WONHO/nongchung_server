190363d9dad92a28a31c16ce0ce28b5b80afc9de
SOGNA <win778500@gmail.com>
2018-07-01 00:45:29 +0900

    add jwt db connection information

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api/index');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
