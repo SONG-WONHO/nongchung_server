96f8445a90a5db719c9f69e2d89f1e915456c7e8
SOGNA <win778500@gmail.com>
2018-07-02 14:29:45 +0900

    almost complete

--- routes/api/home/index.js
+++ routes/api/home/index.js
@@ -6,13 +6,32 @@ const searchRouter = require('./search');
 router.use('/search', searchRouter);
 
 router.get('/', async (req, res, next) => {
-    let selectQuery = `SELECT farm.farm_idx, farm_addr, img, nh_name, nh_price, nh_period, nh_star FROM  NONGHWAL.farm 
-    JOIN NONGHWAL.nh ON farm.farm_idx = nh.farm_idx 
-    JOIN NONGHWAL.farm_img ON farm.farm_idx = farm_img.farm_idx;`
 
+    //광고 관련 회의 필요
+    //인기 농장에 관련한 회의 필요
+    //인기 농활, 뉴 농활에 대한 메서드 만들 필요 있음
+    //대표 이미지 중복 제거 알고리즘 추가 필요
+
+    let selectQuery = `
+    SELECT 
+        nh.nh_idx, 
+        nh.nh_name, 
+        nh.nh_price, 
+        nh.nh_star, 
+        nh.nh_period, 
+        farm.farm_addr, 
+        farm_img.img 
+    FROM NONGHWAL.farm, NONGHWAL.farmer, NONGHWAL.nh, NONGHWAL.farm_img
+    WHERE farm.farmer_idx = farmer.farmer_idx 
+    AND farm.farm_idx = nh.farm_idx
+    AND farm.farm_idx = farm_img.farm_idx`;
+
+    //쿼리 결과 모든 농활 정보
     let selectResult = await db.queryParamNone(selectQuery);
 
-    console.log(selectResult);
+    //인기농활 필터링 쿼리
+
+    //최근농활 필터링 쿼리
 
     if(!selectResult){
         res.status(500).send({
@@ -20,19 +39,58 @@ router.get('/', async (req, res, next) => {
         });
     } else {
         res.status(200).send({
-            message : "Success To get information",
+            message : "Success To Get Information",
             "ads":[
                 {
                     "comment":"맛있는 귤과 함께하는 농활 특집",
-                    "img":"13.13.123.23/ads/1"
+                    "img":"13.13.13.13/ads/1"
                 },
                 {
-                    "comment":"맛있는 사과와 함께하는 농활 특집",
-                    "img":"13.13.123.23/ads/2"
+                    "comment":"우리 고장의 사과를 재배하는 농활 특집",
+                    "img":"13.13.13.13/ads/2"
+                },
+                {
+                    "comment":"사랑이 싹트는 농활 특집",
+                    "img":"13.13.13.13/ads/3"
+                },
+                {
+                    "comment":"여름맞이 아보카도 농활 특집",
+                    "img":"13.13.13.13/ads/4"
+                }
+            ],  
+            "populNh":selectResult,
+            "newNh":[
+                {
+                    "address":"경상북도 경주",
+                    "name":"경주 사과농촌 체험 활동",
+                    "img":"13.13.13.13/farm/3",
+                    "price":"25,000",
+                    "period":"1박 2일",
+                    "star":"8.7"
+                },
+                {
+                    "address":"제주 서귀포시",
+                    "name":"서귀포 행복 감귤농활체험",
+                    "img":"13.13.13.13/farm/4",
+                    "price":"25,000",
+                    "period":"1박 2일",
+                    "star":"8.7"
                 }
             ],
-            "populNh":selectResult,
-            "populFarm":selectResult
+            "populFarm":[
+                {
+                    "address":"제주 서귀포시",
+                    "name":"경주 사과농장"
+                },
+                {
+                    "address":"제주 서귀포시",
+                    "name":"부산 사과농장"
+                },
+                {
+                    "address":"제주 서귀포시",
+                    "name":"대전 포도농장"
+                }
+            ]
         })
     }
 });
