3633e62fb18b8a91f9f0471d6a24aff12e193e0b
kooseolwon <seolwon6289@naver.com>
2018-07-02 13:08:45 +0900

    add changing password and edit point part

--- routes/api/mypage/index.js
+++ routes/api/mypage/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../../module/db');
 const jwt = require('../../../module/jwt');
+const crypto = require('crypto-promise');
 
 //내정보보기
 router.get('/', async (req,res)=>{
@@ -37,9 +38,9 @@ router.get('/', async (req,res)=>{
 router.put('/',async (req,res)=>{
     var token = req.headers.token;
     var nickname = req.body.nickname;
-    if(!token){
+    if(!token || !nickname){
         res.status(400).send({
-            message:"fail to change nickname from client"
+            message:"fail to change nickname from client,Null Value"
         });
     }else{
         var decoded = jwt.verify(token);
@@ -62,6 +63,81 @@ router.put('/',async (req,res)=>{
                     
                 });
             }
+        }
+
+    }
+});
+router.put('/password', async (req,res)=>{
+    var token = req.headers.token;
+    var pw = req.body.password;
+    var newpw = req.body.newpw;
+
+    if(!token || !pw ||!newpw){
+        res.status(400).send({
+            message:"Null value"
+        });
+    }
+    else
+    {
+        var decoded = jwt.verify(token);
+        if(decoded == -1){
+            res.status(500).send({
+                message:"token error"
+            });
+        }else{
+            let checkQuery = "SELECT * FROM user WHERE user_idx = ?";
+            let checkResult = await db.queryParamArr(checkQuery,[decoded.user_idx]);
+            let hashedpw = await crypto.pbkdf2(pw, checkResult[0].user_salt, 100000, 32, 'sha512');
+            if(!checkResult){
+                res.status(500).send({
+                    message:"Internal server error"
+                });
+            }else{
+                if(hashedpw.toString('base64')===checkResult[0].user_pw){
+                    //console.log(hashedpw.toString('base64'));
+                    const salt = await crypto.randomBytes(32);
+                    const hashednewpw = await crypto.pbkdf2(newpw, salt.toString('base64'), 100000, 32, 'sha512');
+                    let newpwQuery = `UPDATE user SET user_pw =?, user_salt=? WHERE user_idx = ?`;
+                    let newpwResult = await db.queryParamArr(newpwQuery,[hashednewpw.toString('base64'),salt.toString('base64'),decoded.user_idx]);
+                    //console.log(hashednewpw.toString('base64'));
+                    if(!newpwResult){
+                        res.status(500).send({
+                            message:"Internal server error"
+                        });
+                    }else{
+                        res.status(200).send({
+                            message:"Success To change PW"
+                        });
+                    }
+                }else{
+                    res.status(400).send({
+                        message:"fail To change PW from client"
+                    });
+                }
+            }
+        }
+    }
+});
+
+router.get('/point',async (req,res)=>{
+    var token = req.headers.token;
+
+    if(!token){
+        res.status(400).send({
+            message:"Null value"
+        });
+    }else{
+        var decoded = jwt.verify(token);
+        if(decoded==-1){
+            res.status(500).send({
+                message:"token error"
+            });
+        }
+        else
+        {
+            let pointQuery = `SELECT * FROM NONGHWAL.shedule AS s, NONGHWAL.nh AS n, NONGHWAL.acitivty AS a WHERE a.sche_idx = s.sche_idx AND s.nh_idx = n.nh_idx`;
+            let pointResult ;
+
         }
 
     }
