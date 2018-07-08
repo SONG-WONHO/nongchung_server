5a9aa84e9651dae936dd6af06f133200d06ef5be
kooseolwon <seolwon6289@naver.com>
2018-07-09 01:21:31 +0900

    add dummy THEMEimg

--- routes/api/home/theme/index.js
+++ routes/api/home/theme/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../../../module/db');
+const jwt = require('../../../../module/jwt');
 
 router.get('/', async (req, res, next) => {
+    let nhTheme = [
+        {
+            "idx":1,
+            "img" :"https://nonghwal.s3.ap-northeast-2.amazonaws.com/user/1531065972519.ab.png"
+            
+        },
+        {
+            "idx":2,
+            "img":"https://nonghwal.s3.ap-northeast-2.amazonaws.com/user/1531065972519.ab.png"
+            
+        },
+        {
+            "idx":3,
+            "img":"https://nonghwal.s3.ap-northeast-2.amazonaws.com/user/1531065972519.ab.png"
+            
+        },
+        {
+            "idx":4,
+            "img":"https://nonghwal.s3.ap-northeast-2.amazonaws.com/user/1531065972519.ab.png"
+            
+        },
+        {
+            "idx":5,
+            "img":"https://nonghwal.s3.ap-northeast-2.amazonaws.com/user/1531065972519.ab.png"
+        }
+    ];//테마별 농활 사진 보내주는 더미데이터
 
-    console.log("Hello world");
+    res.status(200).send({
+        message: "success To show THEME",
+        data:nhTheme
+    });
 });
 
 
