57b6846e14bd8cb6a492a09057ae0873964cc7a1
SOGNA <win778500@gmail.com>
2018-07-14 12:03:24 +0900

    ed

--- module/mongo/newNhSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const newNhSchema = new mongoose.Schema({
-    idx: {type: Number, required: true, unique: true, index: true},
-    data: Object,
-});
-
-module.exports = mongoose.model('NewNh', newNhSchema);
